9e42f36d24183cef5725da3f6c4ef9d4660031cf
jlrandulfe <jlrandulfe@gmail.com>
2017-12-05 19:16:06 +0100

    Update the block design

--- persistence-of-vision.srcs/sources_1/bd/POV_block_design/hdl/POV_block_design.vhd
+++ persistence-of-vision.srcs/sources_1/bd/POV_block_design/hdl/POV_block_design.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2017.2 (lin64) Build 1909853 Thu Jun 15 18:39:10 MDT 2017
---Date        : Tue Dec  5 17:46:32 2017
+--Date        : Tue Dec  5 19:14:07 2017
 --Host        : javi-SAT-L850-Ubuntu running 64-bit Ubuntu 16.04.3 LTS
 --Command     : generate_target POV_block_design.bd
 --Design      : POV_block_design
